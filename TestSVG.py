c3311602e2d104e84dc1e5a4e4b9ab4d1643b8a3
Sergey Titkov <Srg.titkov@gmail.com>
2024-06-28 09:42:42 +0300

    SVG штука мало познанная...

--- TestSVG.py
+++ TestSVG.py
@@ -4,7 +4,7 @@ import sys
 import random
 from lxml import etree
 from PyQt5 import QtCore, QtGui, QtSvg
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QWidget
 
 
 angle = 0
@@ -12,11 +12,10 @@ angle = 0
 def timerEvent():
     global angle
     # set the flowval object text to a number 1-10
-    find_text(root)[0].text = 'BLA'
-    find_text(root)[0].set('transform', "rotate({},3,1)".format(angle))
-    23.811/2
-
-    find_text1(root)[0].set('transform', "rotate({},12.1,16.1)".format(angle,int(svgWidget.width()/2),int(svgWidget.height()/2)))
+    find_text(root)[0].text = 'BLDD'
+    find_text(root)[0].set('x', "30")
+    find_text(root)[0].set('style', 'font: 32px sans-serif; inline-size: 250px;direction: rtl;')
+    find_text1(root)[0].set('transform', "rotate({},75,93)".format(angle,int(svgWidget.width()/2),int(svgWidget.height()/2)))
     print("rotate({} {} {})".format(angle,int(svgWidget.width()/2),int(svgWidget.height()/2)))
     angle = angle + 10
     if angle > 360:
@@ -36,11 +35,13 @@ def timerEvent():
 #    print (root.xpath("//n:text[@id='angle']/n:tspan/text()", namespaces={'n': "http://www.w3.org/2000/svg"}))
 
 
-root = etree.parse(r'flow0.svg')
+
+
+root = etree.parse(r'рисунок-11.svg')
 
 # create an object for the flowval
 find_text = etree.ETXPath("//{http://www.w3.org/2000/svg}text[@id='angle']")
-find_text1 = etree.ETXPath("//{http://www.w3.org/2000/svg}path[@id='path15']")
+find_text1 = etree.ETXPath("//{http://www.w3.org/2000/svg}g[@id='Arrow']")
 
 print (root.xpath("//n:text[@id='angle']/n:tspan/text()", namespaces={'n': "http://www.w3.org/2000/svg"}))
 
@@ -48,7 +49,8 @@ app = QApplication(sys.argv)
 
 svgWidget = QtSvg.QSvgWidget()
 
-svgWidget.load("flow0.svg")
+svgWidget.load(r'рисунок-11.svg')
+svgWidget.setGeometry(300, 300, 200, 300)
 
 # Setup a timer event
 timer = QtCore.QTimer()
