67792a46a7dc10c487386debc07e72c8e2f40465
Sergey Titkov <Srg.titkov@gmail.com>
2025-04-14 11:26:56 +0300

    Добавил нитку для чтения данных из WT

--- TestXML.py
+++ TestXML.py
@@ -1,36 +1,17 @@
-# importing required librarie
-from lxml import etree
+from datetime import datetime
 
-#self.svgWidget.cstTarget = etree.ETXPath("//{http://www.w3.org/2000/svg}text[@id='select-target']")
-#self.svgWidget.cstAngle = etree.ETXPath("//{http://www.w3.org/2000/svg}text[@id='angle']")
-#self.svgWidget.cstArrow = etree.ETXPath("//{http://www.w3.org/2000/svg}g[@id='Arrow']")
-root = etree.parse('MFD - 01___.svg')
-#z = root.find_element_by_xpath("//*[@class='im_editable'][@class='im-chat-input--t']")
-print(root)
-print(etree.tostring(root))
-for element in root.iter():
-    print(f"{element.tag}  - {element.text}")
-z = etree.ETXPath("//{http://www.w3.org/2000/svg}text[starts-with(@id,'sens_')]");
-k = z(root)
-print(k)
-print('-------------')
-for element in k:
-    print(f"{element.tag} - {element.get('id')} - {element.text}")
-    element.text = '!Z!'
-
-print(etree.tostring(root))
-z = etree.ETXPath("//{http://www.w3.org/2000/svg}svg");
-k = z(root)
-print(k)
-print('-------------')
-for element in k:
-    print(f"{element.tag} - {element.get('width')} - {element.get('height')}")
-
-#self.svgWidget.cstTarget(self.svgWidget.cstRoot)[0].text = stroka
-#self.svgWidget.cstAngle(self.svgWidget.cstRoot)[0].text = course_angle_text
-#self.svgWidget.cstArrow(self.svgWidget.cstRoot)[0].set('transform', "rotate({},75,93)".format(int(course_angle)))
-
-
-print('{:1.2f}'.format(-0.000018))
+val = 279.71230018
+print('{:1.2f}'.format(val))
 text_format = ':.0f'
-print(('{'+text_format+'}').format(279.71230018))
+print(('{'+text_format+'}').format(val))
+spec = '1.2f'
+print(f'{val:{spec}}')
+
+current_time = datetime.now()
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+print(current_time.microsecond)
+
+
+
